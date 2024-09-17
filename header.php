8ef13d2d2b03fea3962a2866b6bcfc1557340713
LeeChiMan <61506818+adzli-necius@users.noreply.github.com>
2024-09-17 15:14:35 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,121 @@
+<head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel='stylesheet' type='text/css' href="css/bootstrap.css"/>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/header.css"/>
+	
+</head>
+<header>
+<div class="header">
+	<div class="logo">
+		<a href="index.php">Sistem E-PLK</a>
+		<!-- <img src="Banner.png" alt="Banner Image" class="banner-image"> -->
+
+	</div>
+</div>
+<?php  
+  if (isset($_GET['error'])) {
+		if ($_GET['error'] == "wrongpasswordup") {
+			echo '	<script type="text/javascript">
+					 	setTimeout(function () {
+			                $(".up_info1").fadeIn(200);
+			                $(".up_info1").text("The password is wrong!!");
+			                $("#admin-account").modal("show");
+		              	}, 500);
+		              	setTimeout(function () {
+		                	$(".up_info1").fadeOut(1000);
+		              	}, 3000);
+					</script>';
+		}
+	} 
+	if (isset($_GET['success'])) {
+		if ($_GET['success'] == "updated") {
+			echo '	<script type="text/javascript">
+			 			setTimeout(function () {
+			                $(".up_info2").fadeIn(200);
+			                $(".up_info2").text("Your Account has been updated");
+              			}, 500);
+              			setTimeout(function () {
+                			$(".up_info2").fadeOut(1000);
+              			}, 3000);
+					</script>';
+		}
+	}
+	if (isset($_GET['login'])) {
+	    if ($_GET['login'] == "success") {
+	      echo '<script type="text/javascript">
+	              
+	              setTimeout(function () {
+	                $(".up_info2").fadeIn(200);
+	                $(".up_info2").text("You successfully logged in");
+	              }, 500);
+
+	              setTimeout(function () {
+	                $(".up_info2").fadeOut(1000);
+	              }, 4000);
+	            </script> ';
+	    }
+	  }
+?>
+<div class="topnav" id="myTopnav">
+	<a href="index.php">Pengguna</a>
+    <a href="ManageUsers.php">Urus Pengguna</a>
+    <a href="UsersLog.php">Log Pengguna</a>
+    <a href="devices.php">Peralatan</a>
+    <?php  
+    	if (isset($_SESSION['Admin-name'])) {
+    		echo '<a href="#" data-toggle="modal" data-target="#admin-account">'.$_SESSION['Admin-name'].'</a>';
+    		echo '<a href="logout.php">Log Keluar</a>';
+    	}
+    	else{
+    		echo '<a href="login.php">Log In</a>';
+    	}
+    ?>
+    <a href="javascript:void(0);" class="icon" onclick="navFunction()">
+	  <i class="fa fa-bars"></i></a>
+</div>
+<div class="up_info1 alert-danger"></div>
+<div class="up_info2 alert-success"></div>
+</header>
+<script>
+	function navFunction() {
+	  var x = document.getElementById("myTopnav");
+	  if (x.className === "topnav") {
+	    x.className += " responsive";
+	  } else {
+	    x.className = "topnav";
+	  }
+	}
+</script>
+
+<!-- Kemaskini Akaun Pelajar -->
+<div class="modal fade" id="admin-account" tabindex="-1" role="dialog" aria-labelledby="Admin Update" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h3 class="modal-title" id="exampleModalLongTitle">Update Your Account Info:</h3>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form action="ac_update.php" method="POST" enctype="multipart/form-data">
+	      <div class="modal-body">
+	      	<label for="User-mail"><b>Admin Name:</b></label>
+	      	<input type="text" name="up_name" placeholder="Masukkan nama anda..." value="<?php echo $_SESSION['Admin-name']; ?>" required/><br>
+	      	<label for="User-mail"><b>Admin E-mail:</b></label>
+	      	<input type="email" name="up_email" placeholder="Masukkan E-mel anda..." value="<?php echo $_SESSION['Admin-email']; ?>" required/><br>
+	      	<label for="User-psw"><b>Password</b></label>
+	      	<input type="password" name="up_pwd" placeholder="Masukkan Kata Laluan anda..." required/><br>
+	      </div>
+	      <div class="modal-footer">
+	        <button type="submit" name="update" class="btn btn-success">Save changes</button>
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	      </div>
+	  </form>
+    </div>
+  </div>
+</div>
+<!-- //Kemaskini Akaun Pelajar -->
+	
+
+	
